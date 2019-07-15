b839a2e15620770008645bcf2364e95a08a400f5
tia <tiasteele85@gmail.com>
2019-07-15 10:27:05 -0400

    Tia's first commit

--- src/Midterm.java
+++ src/Midterm.java
@@ -3,8 +3,12 @@ public class Midterm {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
-
+		System.out.println("Once upon a time in the mystical land of Shangrila "
+				+ "there dwelt an immortal named Kai-Yun and "
+				+ "his trusty band of ninja monkeys.");
+		
+		System.out.println("In Kai-Yun's small band of trusty monkeys a new recruit"
+				+ "began to grow increasingly discontent with the glory of the great Kai-Yun.");
 	}
 
 }
