09e8a8e8186f2d66f35c254d666a975751c248e7
ismeil hachem <ismeil27@gmail.com>
2019-07-15 10:56:35 -0400

    Ismeil's second Modification

--- src/Midterm.java
+++ src/Midterm.java
@@ -10,8 +10,12 @@ public class Midterm {
 		System.out.println("In Kai-Yun's small band of trusty monkeys a new recruit"
 				+ "began to grow increasingly discontent with the glory of the great Kai-Yun.");
 		
-		System.out.println("During the discontent there was a woman named Catherine. She met a princess+"
+		System.out.println("During the discontent there was a woman named Catherine. She met a princess"
 				+ "named Tondaleyo. She said when she has a daughter she will name her Tondaleya");
+		
+		System.out.println("During the meeting, Tondaleyo told Catherine that her daughter"
+				+ "will be the one to end the long night."
+				+ "This surprised Catherine because she was still single.");
 	}
 
 }
