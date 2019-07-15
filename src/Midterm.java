a8fcf702bf6cd8083ddfd2b6c5cb79a6f95debf7
Leya Coleman <leyacole99@gmail.com>
2019-07-15 11:19:17 -0400

    conflict

--- src/Midterm.java
+++ src/Midterm.java
@@ -4,11 +4,11 @@ public class Midterm {
 	public static void main(String[] args) {
 		
 		System.out.println("Once upon a time in the mystical land of Shangrila "
-				+ "there dwelt an immortal named Kai-Yun and "
+				+ "there dwelt an immortal named David and "
 				+ "his trusty band of ninja monkeys.");
 		
-		System.out.println("In Kai-Yun's small band of trusty monkeys a new recruit"
-				+ "began to grow increasingly discontent with the glory of the great Kai-Yun.");
+		System.out.println("In David's small band of trusty monkeys a new recruit"
+				+ "began to grow increasingly discontent with the glory of the great David.");
 		
 		System.out.println("During the discontent there was a woman named Catherine. She met a princess"
 				+ "named Tondaleyo. She said when she has a daughter she will name her Tondaleya");
